fd7c48e1c1807e9e6596eac028f3aa110b63610f
Adison Cavani <adrian.srodon@gmail.com>
2021-08-03 18:31:14 +0200

    Update AboutSubPage, add new icons

--- src/Pages/Settings/AboutSubPage.xaml.cs
+++ src/Pages/Settings/AboutSubPage.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +26,78 @@ namespace RSA_WPF.Pages.Settings
         public AboutSubPage()
         {
             InitializeComponent();
+            NETversion.Text = Get45or451FromRegistry();
+            AppVersion.Text = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            WindowsVersion.Text = Environment.OSVersion.ToString();
+        }
+
+        private static string Get45or451FromRegistry()
+        {
+            using (RegistryKey ndpKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32).OpenSubKey("SOFTWARE\\Microsoft\\NET Framework Setup\\NDP\\v4\\Full\\"))
+            {
+                int releaseKey = Convert.ToInt32(ndpKey.GetValue("Release"));
+                return CheckFor45DotVersion(releaseKey);
+            }
+        }
+
+        // Checking the version using >= will enable forward compatibility,  
+        private static string CheckFor45DotVersion(int releaseKey)
+        {
+            if (releaseKey >= 528040)
+            {
+                return "4.8 or later";
+            }
+            if (releaseKey >= 461808)
+            {
+                return "4.7.2 or later";
+            }
+            if (releaseKey >= 461308)
+            {
+                return "4.7.1 or later";
+            }
+            if (releaseKey >= 460798)
+            {
+                return "4.7 or later";
+            }
+            if (releaseKey >= 394802)
+            {
+                return "4.6.2 or later";
+            }
+            if (releaseKey >= 394254)
+            {
+                return "4.6.1 or later";
+            }
+            if (releaseKey >= 393295)
+            {
+                return "4.6 or later";
+            }
+            if (releaseKey >= 393273)
+            {
+                return "4.6 RC or later";
+            }
+            if ((releaseKey >= 379893))
+            {
+                return "4.5.2 or later";
+            }
+            if ((releaseKey >= 378675))
+            {
+                return "4.5.1 or later";
+            }
+            if ((releaseKey >= 378389))
+            {
+                return "4.5 or later";
+            }
+            // This line should never execute. A non-null release key should mean 
+            // that 4.5 or later is installed. 
+            return "No 4.5 or later version detected";
+        }
+
+        private void License_Click(object sender, RoutedEventArgs e)
+        {
+            Process myProcess = new Process();
+            myProcess.StartInfo.UseShellExecute = true;
+            myProcess.StartInfo.FileName = "https://github.com/AdisonCavani/RSA-WPF/blob/master/LICENSE";
+            myProcess.Start();
         }
     }
 }
--- src/Properties/Settings.Designer.cs
+++ src/Properties/Settings.Designer.cs
@@ -9,20 +9,20 @@
 //------------------------------------------------------------------------------
 
 namespace RSA_WPF.Properties {
-
-
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
-
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
+        
         public static Settings Default {
             get {
                 return defaultInstance;
             }
         }
-
+        
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("15")]
@@ -31,7 +31,7 @@ namespace RSA_WPF.Properties {
                 return ((byte)(this["ClearClipboardTime"]));
             }
         }
-
+        
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("2048")]
@@ -40,7 +40,7 @@ namespace RSA_WPF.Properties {
                 return ((uint)(this["KeyLenght"]));
             }
         }
-
+        
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("Dark")]
