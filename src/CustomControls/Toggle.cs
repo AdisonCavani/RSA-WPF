07598d08ac32cd0d3a159c1d6c379f631b5256a2
Adison Cavani <adrian.srodon@gmail.com>
2021-08-13 22:30:17 +0200

    Add new ToggleButton and change Combobox style

---
+++ src/CustomControls/Toggle.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace RSA_WPF
+{
+    public class Toggle : ToggleButton
+    {
+
+        static Toggle()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Toggle), new FrameworkPropertyMetadata(typeof(Toggle)));
+        }
+    }
+}
