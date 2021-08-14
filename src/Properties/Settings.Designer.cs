0ed418506ced77c37c276bf1b6eeccba5c4f3ff7
Adison Cavani <adrian.srodon@gmail.com>
2021-08-14 22:05:09 +0200

    Change default settings

--- src/Properties/Settings.Designer.cs
+++ src/Properties/Settings.Designer.cs
@@ -49,7 +49,7 @@ namespace RSA_WPF.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1")]
         public byte Theme {
             get {
                 return ((byte)(this["Theme"]));
